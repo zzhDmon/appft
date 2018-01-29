3b017f4e449b17a3e29e47aee10d3c0e336cfe2f
zzhDmon <18850148217@163.com>
2018-01-29 10:15:58 +0800

    first 提交

--- www/views/logo/logo.js
+++ www/views/logo/logo.js
@@ -16,24 +16,40 @@ angular.module('App').controller('LogoController',function($timeout,$http,$Facto
 	$scope.canvasinit=function () {
 		fanvas.play("testCanvas", swfData, {
 			cache: true, autoPlay: true,showFPS:0,clearAll:1,scale:1,
-			onFrame: function (index,two) {
+			onFrame: function (index) {
+				console.log(index)
 					if(index == 30){
 							fanvas.pause("testCanvas");
 	                       	setTimeout(function(){
 	                           fanvas.replay("testCanvas");
 	                       	}, 500);
 	                       	// fanvas.gotoAndPlay("testCanvas",35)
+					}else if(index == 65){
+							fanvas.pause("testCanvas");
+							$timeout(function(){
+								// fanvas.gotoAndPlay("testCanvas",35)
+								$state.go('tabs.home')
+							}, 500);
+							// 切换动画
 					}
 			},
 			imagePath: "./"
 		});
 	}
+	//响应式canvas
+	$(window).resize(resizeCanvas);
+	function resizeCanvas() {
+			$('#testCanvas').width($(window).get(0).innerWidth);
+			$('#testCanvas').height($(window).get(0).innerHeight);
+//	        $('#testCanvas').getContext("2d").fillRect(0, 0, canvas.width(), canvas.height());
+	};
+	resizeCanvas();
 
 	$scope.canvasinit();
 
 	$scope.onSwipeUp=function(){
-			// fanvas.gotoAndPlay("testCanvas",30)
-			fanvas.gotoAndStop("testCanvas",40)
+			fanvas.gotoAndPlay("testCanvas",31)
+			// fanvas.gotoAndStop("testCanvas",40)
 	}
 
 })
